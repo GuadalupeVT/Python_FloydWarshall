44e278f261264f5a6ae2a79c68c39b8b11386dda
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-16 01:20:15 -0600

    Se agrego la clase Vertice

--- PruebasFloydWarshall.py
+++ PruebasFloydWarshall.py
@@ -4,30 +4,26 @@ Created on 16/11/2018
 
 @author: GVT
 '''
-class Graph:
+class Grafo:
     def __init__(self):
-        # dictionary containing auxs that map to the corresponding Vertice object
+        
         self.vertices = {}
  
     def agregarVertice(self, aux):
         #Agrega un vertice en el aux al grafo
-        """Add a Vertice with the given aux to the graph."""
         Vertice = Vertice(aux)
         self.vertices[aux] = Vertice
  
     def getVertice(self, aux):
-        """Return Vertice object with the corresponding aux."""
         return self.vertices[aux]
  
     def __contains__(self, aux):
         return aux in self.vertices
  
     def agregar_edge(self, src_aux, dest_aux, weight=1):
-        """Add edge from src_aux to dest_aux with given weight."""
         self.vertices[src_aux].add_neighbour(self.vertices[dest_aux], weight)
  
     def existeEdge(self, src_aux, dest_aux):
-        """Return True if there is an edge from src_aux to dest_aux."""
         return self.vertices[src_aux].does_it_point_to(self.vertices[dest_aux])
  
     def __longitud__(self):
@@ -35,3 +31,23 @@ class Graph:
  
     def __iter__(self):
         return iter(self.vertices.values())
+    
+class Vertice:
+    def __init__(self, aux):
+        self.aux = aux
+        self.points_to = {}
+ 
+    def get_aux(self):
+        return self.aux
+ 
+    def add_neighbour(self, dest, weight):
+        self.points_to[dest] = weight
+ 
+    def get_neighbours(self):
+        return self.points_to.auxs()
+ 
+    def get_pesos(self, dest):
+        return self.points_to[dest]
+ 
+    def apuntaA(self, dest):
+        return dest in self.points_to
