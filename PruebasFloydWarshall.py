a2a77404dc9900c7aca5b1d5e2b0e911bb92c120
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-16 01:16:50 -0600

    Se decidio agregar por clases, se añadio la clase Grafo

--- PruebasFloydWarshall.py
+++ PruebasFloydWarshall.py
@@ -1,33 +1,37 @@
-# http://micaminomaster.com.co/grafo-algoritmo/dijkstra-floyd-warshall-python/
+#https://www.sanfoundry.com/python-program-implement-floyd-warshall-algorithm/
 '''
 Created on 16/11/2018
 
 @author: GVT
 '''
-def floydWarshall(self):
-    nodes = list(self.graph.nodes)
+class Graph:
+    def __init__(self):
+        # dictionary containing auxs that map to the corresponding Vertice object
+        self.vertices = {}
  
-    for i in nodes:
-        dict_i = {}
-        for j in nodes:
-            if i == j:
-                dict_i[j] = 0
-                continue
-            try:
-                dict_i[j] = self.graph[i][j]['weight']
-            except:
-                dict_i[j] = float("inf")
+    def agregarVertice(self, aux):
+        #Agrega un vertice en el aux al grafo
+        """Add a Vertice with the given aux to the graph."""
+        Vertice = Vertice(aux)
+        self.vertices[aux] = Vertice
  
-        self.distances[i] = dict_i
+    def getVertice(self, aux):
+        """Return Vertice object with the corresponding aux."""
+        return self.vertices[aux]
  
-    for i in nodes:
-        for j in nodes:
-            for k in nodes:
-                ij = self.distances[i][j]
-                ik = self.distances[i][k]
-                kj = self.distances[k][j]
+    def __contains__(self, aux):
+        return aux in self.vertices
  
-                if ij > ik + kj:
-                    self.distances[i][j] = ik + kj
+    def agregar_edge(self, src_aux, dest_aux, weight=1):
+        """Add edge from src_aux to dest_aux with given weight."""
+        self.vertices[src_aux].add_neighbour(self.vertices[dest_aux], weight)
  
-    return self.distances
+    def existeEdge(self, src_aux, dest_aux):
+        """Return True if there is an edge from src_aux to dest_aux."""
+        return self.vertices[src_aux].does_it_point_to(self.vertices[dest_aux])
+ 
+    def __longitud__(self):
+        return len(self.vertices)
+ 
+    def __iter__(self):
+        return iter(self.vertices.values())
