43eb3785a2cdb0146d7fb5a73868f0e48c83272b
Guadalupe Vazquez <guvadlt@gmail.com>
2018-11-16 19:48:52 -0600

    Se agrego la clase floyd_warshall parecida a la implementada en Java

--- PruebasFloydWarshall.py
+++ PruebasFloydWarshall.py
@@ -1,3 +1,4 @@
+#https://rosettacode.org/wiki/Floyd-Warshall_algorithm#Python
 '''
 Created on 16/11/2018
 
@@ -6,28 +7,29 @@ Created on 16/11/2018
 from math import inf
 from itertools import product
  
-def floyd_warshall(n, edge):
-    rn = range(n)
-    dist = [[inf] * n for i in rn]
-    nxt  = [[0]   * n for i in rn]
+def floyd_warshall(numVertices, pesos):
+    rn = range(numVertices)
+    #Prepara un arreglo infinito
+    distancia = [[inf] * numVertices for i in rn] 
+    siguiente  = [[0]   * numVertices for i in rn]
     for i in rn:
-        dist[i][i] = 0
-    for u, v, w in edge:
-        dist[u-1][v-1] = w
-        nxt[u-1][v-1] = v-1
-    for k, i, j in product(rn, repeat=3):
-        sum_ik_kj = dist[i][k] + dist[k][j]
-        if dist[i][j] > sum_ik_kj:
-            dist[i][j] = sum_ik_kj
-            nxt[i][j]  = nxt[i][k]
-    print("pair     dist    path")
-    for i, j in product(rn, repeat=2):
+        distancia[i][i] = 0
+    for aux1, aux2, aux3 in pesos:
+        distancia[aux1-1][aux2-1] = aux3
+        siguiente[aux1-1][aux2-1] = aux2-1
+    for k, i, j in product(rn, repetir=3):
+        sum_ik_kj = distancia[i][k] + distancia[k][j]
+        if distancia[i][j] > sum_ik_kj:
+            distancia[i][j] = sum_ik_kj
+            siguiente[i][j]  = siguiente[i][k]
+    print("pair     distancia    path")
+    for i, j in product(rn, repetir=2):
         if i != j:
             path = [i]
             while path[-1] != j:
-                path.append(nxt[path[-1]][j])
+                path.append(siguiente[path[-1]][j])
             print("%d → %d  %4d       %s" 
-                  % (i + 1, j + 1, dist[i][j], 
+                  % (i + 1, j + 1, distancia[i][j], 
                      ' → '.join(str(p + 1) for p in path)))
  
 if __name__ == '__main__':
